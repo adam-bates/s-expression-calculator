0e554bfffd95684d9e48c4a8659004db90049272
Adam Bates <adam@adambates.ca>
2018-09-05 11:54:33 -0400

    Optimized calculation by removing unnecessary method calls and skipping parenthesis removal.

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 Evaluates simplified s-expressions from the command line using python.
 
-Supported functions: `add`, `multiply`
+ Supported functions: `add`, `multiply`
 
 Expression Syntax: `({function} {expression|integer} {expression|integer})`
 
-Examples:
+#### Examples:
 ```
 > python calc.py "123"
 123
@@ -17,3 +17,15 @@ Examples:
 > python calc.py "(multiply 3 (add 1 2))"
 9
 ```
+
+#### How it works:
+
+* **My first thought was recursion. This problem seems perfect for it, as you can recursively
+evaluate expressions, right?** Well sure, but the problem is that in order to evaluate a nested 
+expression, you need to match opening and closing parenthesis of outer functions without
+evaluating inner functions.
+
+* **Instead, I was able to loop through each inner function, starting with the first to close.** 
+This way I could simply find the first instance of `)` and find the nearest `(` before it to put
+together an inner function that contained no nested functions, which was easy to evaluate. Keep 
+doing that until all functions are evaluated, and you end up with the final value.
--- calc.py
+++ calc.py
@@ -5,24 +5,26 @@ class SExpression:
 
     @staticmethod
     def calc(str_input):
-        # 1. Remove surrounding parenthesis
-        if str_input[0] == '(' and str_input[-1] == ')':
-            str_input = str_input[1:-1]
-
-        # 2. Loop through nested expressions and evaluate each
         while ')' in str_input:
             # Start with the first expression to close (ie. first index of ')')
             right_bound = str_input.index(')')
             left_bound = str_input[:right_bound].rindex('(')
 
             # Update str_input, replacing expression with calculated value
+            # Note: Because we're getting the first expression to close,
+            #       it is guaranteed not to have nested functions inside
             value = SExpression._evaluate_single(str_input[left_bound + 1:right_bound])
-            str_input = str_input[:left_bound] + str(value) + str_input[right_bound+1:]
 
-        # 3. Evaluate out-most expression
-        return SExpression._evaluate_single(str_input)
+            if left_bound == 0:
+                return value
+
+            else:
+                str_input = str_input[:left_bound] + str(value) + str_input[right_bound+1:]
+
+        return int(str_input)
 
     @staticmethod
+    # Evaluates simple expression with no nested values
     def _evaluate_single(str_input):
         pieces = str_input.split()
 
